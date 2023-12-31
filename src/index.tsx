96083fed168c3df60315ef20d63bc5a422427b36
Danil Suetin <suetin.danil@gmail.com>
2023-12-31 18:25:16 +0400

    add routing

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  
-
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename="/chamelions">
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
